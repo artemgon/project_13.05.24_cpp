8785590b230615d0e01d4dcf06d22f395eee6ee3
artemgon <artemhoncharuk123@gmail.com>
2024-06-09 09:01:18 +0300

    Creating the first task of practice exercises.

--- main.cpp
+++ main.cpp
@@ -2,5 +2,22 @@
 using namespace std;
 
 int main() {
-	return 0;
+	int min_value, max_value, a;
+	cout << "Please, enter minimum value of range: ";
+	cin >> min_value;
+	cout << "Please, enter max value of range: ";
+	cin >> max_value;
+	cout << "Please, enter the number of this range: ";
+	cin >> a;
+	for (;;)
+	{
+		if (a < min_value || a > max_value)
+		{
+			cout << "Try again: ";
+			cin >> a;
+		}
+		else break;
+	}
+	cout << "Good job!";
+	return 0;                                   
 }
